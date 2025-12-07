8442c6147a3f840938d5f434b6b5a7d9862c3186
Ryan Jensen <rjensen@sourceallies.com>
2025-12-07 01:44:02 -0600

    Day 1 - Part 1

---
+++ 2025/Day 1/SecretEntrance/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+int zeroCount = 0, currentNumber = 50;
+
+foreach (var instruction in input)
+{
+    var clicks = int.Parse(instruction[1..]);
+    var direction = instruction[0] == 'R' ? 1 : -1;
+    var rotation = clicks * direction;
+
+    currentNumber += rotation;
+    currentNumber %= 100;
+
+    if (currentNumber == 0) zeroCount++;
+}
+
+Console.WriteLine("Zero Count: " + zeroCount);
