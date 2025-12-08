e368bbccee182ba0b1f538666cb8eadcbdbef09f
Ryan Jensen <rjensen@sourceallies.com>
2025-12-07 21:47:43 -0600

    Add better documentation

--- 2025/Day 1/SecretEntrance/DialTurner.cs
+++ 2025/Day 1/SecretEntrance/DialTurner.cs
@@ -16,7 +16,8 @@ public class DialTurner
             var rotation = clicks * direction;
    
             _currentNumber += rotation;
-            _currentNumber = (_currentNumber % 100 + 100) % 100;
+            
+            WrapAroundTheDial();
    
             if (_currentNumber == 0) _zeroCount++;
         }
@@ -26,6 +27,12 @@ public class DialTurner
    
     /// <summary>
     /// Part 2
+    /// Increment zeroCound for three scenarios:
+    /// <list type="bullet">
+    ///     <item>The number of clicks is greater than 100, and you can count each full rotation</item>
+    ///     <item>After rotating the dial, you've passed 0</item>
+    ///     <item>You land on zero</item>
+    /// </list>
     /// </summary>
     public int CountEveryPassingZero(string[] instructions)
     {
@@ -39,15 +46,28 @@ public class DialTurner
             var rotation = (clicks - fullRotations * 100) * direction;
 
             _currentNumber += rotation;
-            _zeroCount += _currentNumber < 0 && _currentNumber != rotation ? 1 : _currentNumber > 100 ? _currentNumber / 100 : 0;
-            _currentNumber = (_currentNumber % 100 + 100) % 100;
+            
+            var hasPassedZero = (_currentNumber < 0 && _currentNumber != rotation) || _currentNumber > 100;
+            _zeroCount += hasPassedZero ? 1 : 0;
 
+            WrapAroundTheDial();
+            
             if (_currentNumber == 0) _zeroCount++;
         }
 
         return _zeroCount;
     }
 
+    /// <summary>
+    /// Pull the number of clicks and direction from the instruction string in that order
+    /// </summary>
+    /// <returns>A tuple containing the number of clicks and direction</returns>
     private static (int, int) ParseInstruction(string instruction) =>
         (int.Parse(instruction[1..]), instruction[0] == 'R' ? 1 : -1);
+    
+    /// <summary>
+    /// C# doesn't modulo like other languages, so we have to manually wrap around the dial
+    /// Otherwise, I'd just use <c>_currentNumber %= 100;</c>
+    /// </summary>
+    private void WrapAroundTheDial() => _currentNumber = (_currentNumber % 100 + 100) % 100;
 }
